cedfb4eb7c5f4deddd50db177024f0676eebb45a
Matheus <matheusgaldinoinfo@gmail.com>
2022-05-03 18:52:18 -0300

    Adicionando dependencias e mapeamento DB

--- src/main/java/com/dio/santander/bankline/api/model/Conta.java
+++ src/main/java/com/dio/santander/bankline/api/model/Conta.java
@@ -1,5 +1,30 @@
 package com.dio.santander.bankline.api.model;
 
-public class Conta {
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
 
+@Embeddable
+public class Conta {
+	@Column(name="Conta_numero")
+	private Long numero;
+	@Column(name="Conta_saldo")
+	private double saldo;
+	
+	
+	
+	public Long getNumero() {
+		return numero;
+	}
+	public void setNumero(Long numero) {
+		this.numero = numero;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	
+	
 }
